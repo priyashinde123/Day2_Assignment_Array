5aa9d3872260b728efe8b234533c89d81ac1eabc
priyashinde123 <113039144+priyashinde123@users.noreply.github.com>
2022-09-08 22:38:24 +0530

    Add files via upload

---
+++ Q9.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Array
+{
+    internal class Q9
+    {
+        public static void Execute()
+        {
+            int[] a = new int[5];
+            int minval = int.MaxValue;
+            int maxval=int.MinValue;
+            Console.WriteLine("enter the elemeints in array");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i]<minval)
+                    minval = a[i];
+                if(a[i]>maxval)
+                    maxval = a[i];
+            }
+
+            Console.WriteLine($"Minimum value :{minval}");
+            Console.WriteLine($"Maximum value : {maxval}");
+        }
+    }
+}
